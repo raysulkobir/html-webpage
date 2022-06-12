684be1f30bb8ac25fa98a56d4f01e925b9d49cab
raysulkobir <raysulkobir97@gmail.com>
2022-06-12 20:29:26 +0600

    first commit

---
+++ 4.3 - Sanitize user input.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        if(isset($_POST['name']) && !empty($_POST['name'])) {
+            echo "My Name: " . htmlspecialchars($_POST['name']) . "!".PHP_EOL;
+        } 
+
+        if(isset($_POST['email']) && !empty($_POST['email'])) {
+            echo "My Email: " . htmlspecialchars($_GET['email']) . "!".PHP_EOL;
+        }
+        
+        
+    ?>
+
+    <form method="POST">
+        <label for="name">Name:</label>
+        <input type="text" name="name">
+        <br>
+        <br>
+        <label for="email">Email:</label>
+        <input type="text" name="email">
+        <br>
+        <br>
+        <label for="message">Message:</label>
+        <textarea name="message" rows="5" cols="30"></textarea>
+        <br>
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+
+   
+</body>
+</html>
